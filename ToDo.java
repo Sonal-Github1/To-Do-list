9d97f622dd8483d42bcce57314a9cdba7c31f993
first-Git <126142847+Sonal-Github1@users.noreply.github.com>
2024-04-23 22:35:22 +0530

    modification

--- ToDo.java
+++ ToDo.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Scanner;//#tharindu
 
 public class ToDo {
     private ArrayList<String> tasks;//initializing array list
@@ -10,11 +10,11 @@ public class ToDo {
         tasks = new ArrayList<>();//this is a instance of above arraylist called tasks
     }
 
-    public void addTask(String task) {
+    public void add(String task) {
         tasks.add(task);//this method use build in .add function to add tasks to the array list
     }
 
-    public void removeTask(int index) {
+    public void remove(int index) {
         if (index >= 0 && index < tasks.size()) {
             tasks.remove(index);//logic says if index of a task bigger than or equal to 0(not negative) AND size of the arraylist bond with index.
         } else {
@@ -22,7 +22,7 @@ public class ToDo {
         }
     }
 
-    public void displayTasks() {
+    public void display() {
         if (tasks.isEmpty()) {
             System.out.println("No tasks in the list.");//this check if input empty or not by .isEmpty built in method 
         } else {
@@ -31,49 +31,52 @@ public class ToDo {
                 System.out.println((i + 1) + ". " + tasks.get(i));//this built-in .get method use to display the previously added task
             }
         }
-    }
+    }//binaksha
 
     public static void main(String[] args) {// this is where program start to execute
         ToDo ToDo = new ToDo();// instance of todo class to get access to above methods
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
-
+        
         while (running) {
             System.out.println("\nTodo List Program");
-            System.out.println("1. Add Task");
-            System.out.println("2. Remove Task");
-            System.out.println("3. Display Tasks");
-            System.out.println("4. Exit");
-            System.out.print("Enter your choice (Num): ");
+            System.out.println("[1] Add Task");
+            System.out.println("[2] Display Tasks");
+            System.out.println("[3] Delete Task");
+            System.out.println("[4] Exit");
+            System.out.print("Enter your choice [Num]: ");
             int choice;
 
         try {
             choice = scanner.nextInt();
             scanner.nextLine();
         } catch (Exception e) {
-            System.out.println("Invalid input");
+            System.out.println("Invalid input enter valid number");
             scanner.nextLine();
             continue; 
-        }
+        }//madhawa
 
             switch (choice) {
                 case 1:
                     System.out.print("Enter task to add: ");
                     String newTask = scanner.nextLine();
-                    ToDo.addTask(newTask);// uses the addtask method
+                    ToDo.add(newTask);// uses the add method
                     System.out.println("Task added.");
                     break;
                 case 2:
+                    ToDo.display();
+                    break;// uses the displaytask method
+                    
+                case 3:
                     System.out.print("Enter index of task to remove: ");
                     int indexToRemove = scanner.nextInt() - 1;
-                    ToDo.removeTask(indexToRemove);// uses the removetask method
+                    ToDo.remove(indexToRemove);// uses the remove method
                     System.out.println("Task removed.");
                     break;
-                case 3:
-                    ToDo.displayTasks();
-                    break;// uses the displaytask method
+                    
                 case 4:
                     running = false;
+                    System.out.println("Thank you for using me!");
                     System.out.println("Exiting...");
                     break;
                 default:
@@ -81,6 +84,6 @@ public class ToDo {
                     break;
             }
         }
-        scanner.close();
+        scanner.close();//sonal
     }
 }
